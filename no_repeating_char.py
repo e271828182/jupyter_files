ef481bbb1e72501fc8f8027f16feaf22907a284e
e271828182 <e2.718@live.com>
2019-07-08 17:09:06 +0800

    init

---
+++ no_repeating_char.py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.1.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        begin = 0
+        end = 0
+        temp_dict = {}
+        ll = 0
+        for i, e in enumerate(s):
+            if e in temp_dict:
+                begin = max(begin,temp_dict[e]+1)
+                temp_dict[e] = i
+            else:
+                temp_dict[e] = i
+            end = i
+            l = end - begin + 1
+            print(end,begin)
+            if l > ll:
+                ll = l
+        return ll
+
+
+# %%
+s = Solution()
+r = s.lengthOfLongestSubstring('abba')
+r
---
+++ twosum.py
@@ -0,0 +1,30 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.1.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        temp_dict = {}
+        for i, x in enumerate(nums):
+            if x not in temp_dict:
+                temp_dict[x] = i
+            y = target - x
+            if y in temp_dict and temp_dict[y] != i:
+                return [temp_dict[y],i]
+
+
+# %%
+s = Solution()
+s.twoSum([3,3],6)
